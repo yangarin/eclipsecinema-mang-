12bc6bc92fc2f5682b717e3abb615b0a9eb24c44
yangarin <yangarin@naver.com>
2016-09-21 15:45:47 +0900

    adminnoticeVO 추가 코딩 , servlet-context 어노테이션 설정 , root-context DataSource , Mybatis ,transactionManager 설정, web.xml 한글처리 추가

---
+++ EclipseCinema/src/main/java/com/cinema/adminnotice/Ad_NoticeVO.java
@@ -0,0 +1,42 @@
+package com.cinema.adminnotice;
+
+public class Ad_NoticeVO {
+
+	private int n_num=0; //글번호
+	private String n_name=""; //올린이
+	private String n_title=""; //글제목
+	private String n_content=""; //내용
+	private String n_date=""; //등록 날짜
+	public int getN_num() {
+		return n_num;
+	}
+	public void setN_num(int n_num) {
+		this.n_num = n_num;
+	}
+	public String getN_name() {
+		return n_name;
+	}
+	public void setN_name(String n_name) {
+		this.n_name = n_name;
+	}
+	public String getN_title() {
+		return n_title;
+	}
+	public void setN_title(String n_title) {
+		this.n_title = n_title;
+	}
+	public String getN_content() {
+		return n_content;
+	}
+	public void setN_content(String n_content) {
+		this.n_content = n_content;
+	}
+	public String getN_date() {
+		return n_date;
+	}
+	public void setN_date(String n_date) {
+		this.n_date = n_date;
+	}
+	
+	
+}
