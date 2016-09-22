c5e2b70e7bc9fd0f873cd8d70087e85faa880e58
yeongbinJo <popoyb@naver.com>
2016-09-22 13:50:59 +0900

    회원로그인 mapper.xml 회원가입, 회원정보 불러오기, 아이디비밀번호찾기 쿼리문 작성 VO에 이름 추가 로그인 컨트롤러 파일 추가

--- EclipseCinema/src/main/java/com/cinema/usermembership/User_MembershipVO.java
+++ EclipseCinema/src/main/java/com/cinema/usermembership/User_MembershipVO.java
@@ -3,17 +3,48 @@ package com.cinema.usermembership;
 public class User_MembershipVO {
 
 	private String C_ID = ""; // 고객 아이디
+	private String C_NAME = ""; // 고객이름
 	private String C_PW = ""; //  고객 비밀번호
 	private String C_PHONE = ""; //  고객 전화번호
 	private String C_BIRTH = ""; //  고객 생일
 	private String C_GENDER = "";  // 고객 성별
 	private int C_MILEAGE = 0; // 고객 마일리지  
+	
+	public User_MembershipVO(){
+		
+	}
+	
+	
+	
+	public User_MembershipVO(String c_ID, String c_NAME, String c_PW, String c_PHONE, String c_BIRTH, String c_GENDER,
+			int c_MILEAGE) {
+		super();
+		C_ID = c_ID;
+		C_NAME = c_NAME;
+		C_PW = c_PW;
+		C_PHONE = c_PHONE;
+		C_BIRTH = c_BIRTH;
+		C_GENDER = c_GENDER;
+		C_MILEAGE = c_MILEAGE;
+	}
+
+
+
 	public String getC_ID() {
 		return C_ID;
 	}
 	public void setC_ID(String c_ID) {
 		this.C_ID = c_ID;
 	}
+	
+	public String getC_NAME() {
+		return C_NAME;
+	}
+
+	public void setC_NAME(String c_NAME) {
+		this.C_NAME = c_NAME;
+	}
+
 	public String getC_PW() {
 		return C_PW;
 	}
